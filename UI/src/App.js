fc13bafda93f60ee6a411c30bde2141ba2567189
Arushi Agarwal <aaruagarwal15@gmail.com>
2020-12-10 12:01:21 +0530

    UI+server

---
+++ UI/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ContentAnalyzer from './containers/contentAnalyzer/contentAnalyzer';
+
+function App() {
+  return (
+    <div className="App">
+      <ContentAnalyzer />
+    </div>
+  );
+}
+
+export default App;
